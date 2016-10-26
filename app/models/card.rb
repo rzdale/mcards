2f25d5b7b5947e147a269533b74183137e932af7
robzd1 <robzd1@gmail.com>
2016-10-26 22:21:41 +0000

    Add checkout page and order table/processing

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,3 +1,11 @@
 class Card < ActiveRecord::Base
 
+    def price
+        price = case self.rarity
+            when 1 then 1.0
+            when 2 then 5.0
+            when 3 then 10.0
+        end
+        price
+    end
 end
